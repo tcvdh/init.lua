d608a23421b8214d794454568bbf3daab09443ba
tcvdh <tcvdh@outlook.com>
2025-03-30 00:25:11 +0100

    add mini.animate

---
+++ lua/tcvdh/lazy/mini.lua
@@ -0,0 +1,18 @@
+return {
+    "echasnovski/mini.nvim",
+    config = function()
+        require('mini.animate').setup({
+            -- cursor = {
+            --      enable = true,
+            --      speed = 10,
+            --      delay = 0.1,
+            --  },
+            --  scroll = {
+            --      enable = true,
+            --      speed = 10,
+            --      delay = 0.1,
+            --  },
+        })
+    end
+}
+
