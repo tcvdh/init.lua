2d114a41e822d55a6dd7898028eef6e7e76a5487
tcvdh <tcvdh@outlook.com>
2025-04-17 18:05:57 +0200

    set vimtex viewer for macos and linux seperately

--- lua/tcvdh/lazy/vimtex.lua
+++ lua/tcvdh/lazy/vimtex.lua
@@ -4,6 +4,13 @@ return {
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = "zathura"
+    -- vim.g.vimtex_view_method = "sioyek"
+    if vim.fn.has('mac') == 1 then
+      -- macOS configuration
+      vim.g.vimtex_view_method = "sioyek"
+    else
+      -- Linux configuration (default)
+      vim.g.vimtex_view_method = "zathura"
+    end
   end
 }
